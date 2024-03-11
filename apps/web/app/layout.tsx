a301014bbed8df378d638a7356b0370577eb035a
Ilia Byzov <17595195+vuode@users.noreply.github.com>
2024-03-12 00:42:48 +0100

    fix: ios 100vh

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -15,7 +16,7 @@ export default function RootLayout({
 }): JSX.Element {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={clsx(inter.className, "h-screen")}>{children}</body>
     </html>
   );
 }
