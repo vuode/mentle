e5821732998f49ad5e2857f799a64a44d9dc5c61
Ilia Byzov <17595195+vuode@users.noreply.github.com>
2024-03-14 21:46:52 +0100

    feat: add utils

---
+++ packages/utils/src/getNumberFromToken.ts
@@ -0,0 +1,37 @@
+import crc32 from "crc-32";
+
+interface GetNumberFromTokenOptions {
+  seed?: number;
+  range?: {
+    from: number;
+    to: number;
+    exclude?: RegExp;
+  };
+}
+
+export const getNumberFromToken = (
+  token: string,
+  options?: GetNumberFromTokenOptions,
+) => {
+  const hash = Math.abs(crc32.str(token, options?.seed));
+
+  if (options?.range) {
+    const { from, to, exclude } = options.range;
+
+    const length = to - from + 1;
+
+    if (exclude) {
+      const range = Array.from({ length }).flatMap((_value, index) => {
+        const value = index + from;
+
+        return exclude.test(value.toString(10)) ? [] : value;
+      });
+
+      return range[hash % range.length] as number;
+    }
+
+    return (hash % length) + from - 1;
+  }
+
+  return hash;
+};
