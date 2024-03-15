91a4fb4644ff2dbd8fb47e69981b9794a8c2c58f
Ilia Byzov <17595195+vuode@users.noreply.github.com>
2024-03-15 23:44:08 +0100

    feat: add form fields

---
+++ apps/web/components/form/InputField.tsx
@@ -0,0 +1,44 @@
+import { useField, FieldInputProps } from "react-final-form";
+import { Input, InputProps } from "@repo/ui/input";
+
+export interface InputFieldProps
+  extends Exclude<InputProps, keyof FieldInputProps<string, HTMLElement>> {
+  name: string;
+  label?: string;
+  showError?: boolean;
+}
+
+export const InputField: React.FC<InputFieldProps> = ({
+  name,
+  label,
+  showError,
+  ...props
+}) => {
+  const { input, meta } = useField<string>(name);
+
+  if (label) {
+    return (
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <label
+          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          htmlFor={`${name}Field`}
+        >
+          {label}
+        </label>
+        <Input {...props} {...input} id={`${name}Field`} />
+        {showError && (
+          <div className="h-5 text-xs text-red-500">{meta.error}</div>
+        )}
+      </div>
+    );
+  }
+
+  return (
+    <div className="grid w-full max-w-sm items-center gap-1.5">
+      <Input {...input} {...props} />
+      {showError && (
+        <div className="h-5 text-xs text-red-500">{meta.error}</div>
+      )}
+    </div>
+  );
+};
---
+++ apps/web/components/form/NumberInputField.tsx
@@ -0,0 +1,56 @@
+import { useField, FieldInputProps } from "react-final-form";
+import { NumberInput, NumberInputProps } from "@repo/ui/input";
+
+export interface NumberInputFieldProps
+  extends Exclude<
+    NumberInputProps,
+    keyof FieldInputProps<string, HTMLElement>
+  > {
+  name: string;
+  label?: string;
+  showError?: boolean;
+}
+
+export const NumberInputField: React.FC<NumberInputFieldProps> = ({
+  name,
+  label,
+  showError,
+  ...props
+}) => {
+  const {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    input: { type, onChange, ...input },
+    meta,
+  } = useField<number>(name);
+
+  if (label) {
+    return (
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <label
+          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          htmlFor={`${name}Field`}
+        >
+          {label}
+        </label>
+        <NumberInput
+          {...props}
+          {...input}
+          id={`${name}Field`}
+          onValueChange={onChange}
+        />
+        {showError && (
+          <div className="h-5 text-xs text-red-500">{meta.error}</div>
+        )}
+      </div>
+    );
+  }
+
+  return (
+    <div className="grid w-full max-w-sm items-center gap-1.5">
+      <NumberInput {...input} {...props} onValueChange={onChange} />
+      {showError && (
+        <div className="h-5 text-xs text-red-500">{meta.error}</div>
+      )}
+    </div>
+  );
+};
