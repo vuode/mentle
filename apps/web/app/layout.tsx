18ea6191f58545101700700b59b02c4fdc282589
Ilia Byzov <17595195+vuode@users.noreply.github.com>
2024-03-16 20:33:13 +0100

    fix: zoom on small text inputs

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,12 +1,18 @@
 import clsx from "clsx";
 import "./globals.css";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Mentle",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
 };
 
 export default function RootLayout({
